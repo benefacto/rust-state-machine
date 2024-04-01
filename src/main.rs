7aa83a57d899b0978300d08ec0f7a83ab1af17ad
Ben Walker <ben.walker18@gmail.com>
2024-04-01 15:01:54 -0700

    Implement runtime

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,23 @@ impl Runtime {
 
 fn main() {
 	let mut runtime = Runtime::new();
-	runtime.balances.set_balance("alice".to_owned(), 100);
-	// TODO
+	let alice = "alice";
+	runtime.balances.set_balance(alice.to_owned(), 100);
+
+	let block_number = runtime.system.block_number();
+	let inc_block_number_result = runtime.system.inc_block_number();
+	assert!(inc_block_number_result.is_ok());
+	assert!(runtime.system.block_number() > block_number);
+
+	let nonce = runtime.system.nonce(alice);
+	let inc_nonce_result = runtime.system.inc_nonce(alice.to_owned());
+	assert!(inc_nonce_result.is_ok());
+	assert!(runtime.system.nonce(alice) > nonce);
+	let transfer_result = runtime.balances.transfer(alice.to_owned(), "bob".to_owned(), 30);
+	assert!(transfer_result.is_ok());
+
+	let inc_nonce_result = runtime.system.inc_nonce(alice.to_owned());
+	assert!(inc_nonce_result.is_ok());
+	let transfer_result = runtime.balances.transfer(alice.to_owned(), "charlie".to_owned(), 30);
+	assert!(transfer_result.is_ok());
 }
