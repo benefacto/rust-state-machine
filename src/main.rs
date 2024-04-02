c5c8e5a23827da680d9c4eeb3571c12ff1f5c8d4
Ben Walker <ben.walker18@gmail.com>
2024-04-01 19:24:34 -0700

    Add proof of existence

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod balances;
+mod proof_of_existence;
 mod support;
 mod system;
 
@@ -12,16 +13,19 @@ mod types {
 	pub type Extrinsic = crate::support::Extrinsic<AccountId, crate::RuntimeCall>;
 	pub type Header = crate::support::Header<BlockNumber>;
 	pub type Block = crate::support::Block<Header, Extrinsic>;
+	pub type Content = &'static str;
 }
 
 pub enum RuntimeCall {
 	Balances(balances::Call<Runtime>),
+	ProofOfExistence(proof_of_existence::Call<Runtime>),
 }
 
 #[derive(Debug)]
 pub struct Runtime {
 	system: system::Pallet<Self>,
 	balances: balances::Pallet<Self>,
+	proof_of_existence: proof_of_existence::Pallet<Self>,
 }
 
 impl system::Config for Runtime {
@@ -34,11 +38,18 @@ impl balances::Config for Runtime {
 	type Balance = u128;
 }
 
+impl proof_of_existence::Config for Runtime {
+	type Content = types::Content;
+}
+
 impl Runtime {
 	fn new() -> Self {
-		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+		Self {
+			system: system::Pallet::new(),
+			balances: balances::Pallet::new(),
+			proof_of_existence: proof_of_existence::Pallet::new(),
+		}
 	}
-
 
 	fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
 		let _ = self.system.inc_block_number();
@@ -59,25 +70,27 @@ impl Runtime {
 }
 
 impl crate::support::Dispatch for Runtime {
-    type Caller = <Runtime as system::Config>::AccountId;
-    type Call = RuntimeCall;
+	type Caller = <Runtime as system::Config>::AccountId;
+	type Call = RuntimeCall;
 
-    fn dispatch(
-        &mut self,
-        caller: Self::Caller,
-        runtime_call: Self::Call,
-    ) -> support::DispatchResult {
-        match runtime_call {
-            RuntimeCall::Balances(call) => match call {
-                balances::Call::Transfer { to, amount } => {
-                    self.balances.transfer(caller, to, amount)?
-                },
-            },
-        }
-        Ok(())
-    }
+	fn dispatch(
+		&mut self,
+		caller: Self::Caller,
+		runtime_call: Self::Call,
+	) -> support::DispatchResult {
+		match runtime_call {
+			RuntimeCall::Balances(call) => match call {
+				balances::Call::Transfer { to, amount } => {
+					self.balances.transfer(caller, to, amount)?
+				},
+			},
+			RuntimeCall::ProofOfExistence(call) => {
+				self.proof_of_existence.dispatch(caller, call)?;
+			},
+		}
+		Ok(())
+	}
 }
-
 
 fn main() {
 	let mut runtime = Runtime::new();
@@ -92,16 +105,54 @@ fn main() {
 		extrinsics: vec![
 			support::Extrinsic {
 				caller: alice.clone(),
-				call: RuntimeCall::Balances(balances::Call::Transfer { to: bob, amount: 20 }),
+				call: RuntimeCall::Balances(balances::Call::Transfer { to: bob.clone(), amount: 20 }),
 			},
 			support::Extrinsic {
-				caller: alice,
+				caller: alice.clone(),
 				call: RuntimeCall::Balances(balances::Call::Transfer { to: charlie, amount: 20 }),
 			},
 		],
 	};
+	let block_2 = types::Block {
+		header: support::Header { block_number: 2 },
+		extrinsics: vec![
+			support::Extrinsic {
+				caller: alice.clone(),
+				call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::CreateClaim {
+					claim: &"Hello, world!",
+				}),
+			},
+			support::Extrinsic {
+				caller: bob.clone(),
+				call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::CreateClaim {
+					claim: &"Hello, world!",
+				}),
+			},
+		],
+	};
+	let block_3 = types::Block {
+		header: support::Header { block_number: 3 },
+		extrinsics: vec![
+			support::Extrinsic {
+				caller: alice,
+				call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::RevokeClaim {
+					claim: &"Hello, world!",
+				}),
+			},
+			support::Extrinsic {
+				caller: bob,
+				call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::CreateClaim {
+					claim: &"Hello, world!",
+				}),
+			},
+		],
+	};
+
+	
 	
 	runtime.execute_block(block_1).expect("invalid block");
+	runtime.execute_block(block_2).expect("invalid block");
+	runtime.execute_block(block_3).expect("invalid block");
 
 	println!("{:#?}", runtime);
 }
